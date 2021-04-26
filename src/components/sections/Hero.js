1bed9f03e48bb1b54e7120a0df42dfac7929e3ea
James Coholan <jamescoholan@gmail.com>
2021-04-26 15:31:09 -0700

    commit on 'main'

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -104,7 +104,7 @@ const Hero = ({
             >
               <Image
                 className="has-shadow"
-                src={require("./../../assets/images/video-placeholder.jpg")}
+                src={require("./../../assets/images/111.png")}
                 alt="Hero"
                 width={896}
                 height={504}
