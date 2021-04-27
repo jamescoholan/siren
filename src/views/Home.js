fd0ab217c1c2870c9d108ca7481c5d763e977bba
James Coholan <jamescoholan@gmail.com>
2021-04-26 17:46:13 -0700

    commit on 'main'

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,6 +5,7 @@ import FeaturesTiles from "../components/sections/FeaturesTiles";
 import FeaturesSplit from "../components/sections/FeaturesSplit";
 import Testimonial from "../components/sections/Testimonial";
 import Cta from "../components/sections/Cta";
+import Hero2 from "../components/sections/Hero2";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
         imageFill
         className="illustration-section-02"
       />
+      <Hero2 />
       {/* <Testimonial topDivider /> */}
       <Cta split />
     </>
