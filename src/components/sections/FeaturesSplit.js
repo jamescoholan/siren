7851a8848fb317ea89e1bf72e8d4dbd096006ff8
James Coholan <jamescoholan@gmail.com>
2021-04-26 18:55:12 -0700

    commit on 'main'

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -49,9 +49,8 @@ const FeaturesSplit = ({
   );
 
   const sectionHeader = {
-    title: "Workflow that just works",
-    paragraph:
-      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.",
+    title: "The Great Unlock",
+    paragraph: "Maximizing Access to the Best Information",
   };
 
   return (
